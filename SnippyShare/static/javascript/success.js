2beb4d679fc92bc635ebb1bfb11c42c42bbd8c9f
Kavoyaa <epickavoya123@gmail.com>
2024-09-07 23:53:10 +0530

    Fix slicing (fr this time)

--- SnippyShare/static/javascript/success.js
+++ SnippyShare/static/javascript/success.js
@@ -2,5 +2,5 @@ var copyBtn = document.getElementById("copy-btn");
 var copyText = document.getElementById("copy-text");
 
 copyBtn.onclick = function () {
-    navigator.clipboard.writeText(document.location + copyText.getAttribute("href").slice(1));
+    navigator.clipboard.writeText(document.location.href + copyText.getAttribute("href").slice(1));
 };
